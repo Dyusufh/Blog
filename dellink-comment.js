5db6d7c1cc7a93e471df860c8a7f099d1231ebca
Bandi Shippuden <bandi.shippuden@gmail.com>
2017-08-26 23:57:49 +0800

    Version
    ver-001

---
+++ dellink-comment.js
@@ -0,0 +1,2 @@
+jQuery(document).ready(function(){jQuery(".cm_body p").find("a").replaceWith("<span style="color: red;"><b><i>Spam !!!</i></b></span><mark>Note:</mark> <i>Link aktif otomatis terhapus ...</i>");
+});
